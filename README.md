0431cc970b97c6791093495721d2d5610b1ae12f
saxton <saxton@illinois.edu>
2022-03-25 15:33:30 -0500

    adding docstrings and expanding README

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # jlDistributedQuery
 
+**DistributedQuery** Is meant to host large datasets in partitioned across multiple workers with queryable access. Idealy, worker will be remote worker with their own memory. has 3 major part, `deployDataStore()`, `sentinal()`, `make_query_channels()`, `query_client()`. The basic exicution is the following,
+
+```
+using Test
+using Distributed
+using ClusterManagers
+using DistributedQuery
+
+proj_path = joinpath(["/", split(Base.current_project(), "/")[1:end-1]...])
+p = addprocs(SlurmManager(3),
+             time="00:30:00",
+             exeflags="--project=$(proj_path)", ntasks_per_node=1)
+
+@everywhere using DistributedQuery
+@everywhere using DataFrames
+@everywhere using CSV
+#proc_chan, data_chan = make_query_channels(p, [1], chan_depth::Int=5);
+
+_shard_file_list = ["../mockData/iris_df_1.jlb",
+                    "../mockData/iris_df_2.jlb",
+                    "../mockData/iris_df_3.jlb"]
+
+shard_file_list = [joinpath(dirname(pathof(DistributedQuery)), sf) for sf in _shard_file_list]
+serialized_file_list = shard_file_list
+data_worker_pool = p
+proc_worker_pool = [myid()]
+fut = DistributedQuery.deployDataStore(data_worker_pool, proc_worker_pool, serialized_file_list)
+
+test_res = [fetch(fut[p]) == @fetchfrom p DistributedQuery.DataContainer for p in data_worker_pool]
+
+@test all(test_res)
+
+if all(test_res)
+    print("DistributedQuery.deployDataStore passed\n")
+else
+    print("DistributedQuery.deployDataStore Failed\n test_res: $(test_res)")
+end
+
+
+proc_chan, data_chan = DistributedQuery.make_query_channels(data_worker_pool, proc_worker_pool)
+
+status_chan = RemoteChannel(()->Channel{Any}(10000), myid())
+
+sentinal_fut =
+    [@spawnat p DistributedQuery.sentinal(DistributedQuery.DataContainer,
+                                          data_chan[myid()] ,proc_chan,
+                                          status_chan)
+     for p in data_worker_pool]
+
+rmprocs(p);
+
+```
--- src/DistributedQuery.jl
+++ src/DistributedQuery.jl
@@ -5,8 +5,23 @@ using DataFrames
 using CSV
 
 """
+
+    deployDataStore(data_worker_pool, serialized_file_list)
+
+deployDataStore assumes your have already a number of serialized files that contain partitions of your dataset. deserialize() will be run on the files that you pass to serialize_file_list and the result will assigned to DistributedQuery.DataContainer. As a result the hosting model demands that data_worker_pool and serialized_file_list must be the same length
+
+# Arguments
+- `data_worker_pool::Any`: Array of worker ids that each of the data partitions will be placed on
+- `serialized_file_list::Any`: Array of strings that contain the path and filename to the serialized file
+
+# Returns
+- `Dict{Any, Future}`: Returns a Ditionary with worker id as the key, and Future of the spawend deserialize task
+
+# Throws
+- `ERROR`: Thrown if length(data_worker_pool) != length(serialized_file_list)
+
 """
-function deployDataStore(data_worker_pool, proc_worker_pool, serialized_file_list)
+function deployDataStore(data_worker_pool, serialized_file_list)
     if length(data_worker_pool) != length(serialized_file_list)
         throw(error("""data_worker_pool and serialized_file_list must be the same length: """*
                     """length(data_worker_pool):$(length(data_worker_pool)), """*
@@ -21,14 +36,30 @@ function deployDataStore(data_worker_pool, proc_worker_pool, serialized_file_lis
     #[wait(f) for f in fut]
 end
 
-"""
-Sentinal waits for a item to be put on the q_channel.
-
-The item put in q_channel should either be a String with the message "Done". This will cause sentinal() to exit it's  while loop and truen.
-
 
 """
-function sentinal(DataContainer, q_channel,res_channel_dict, status_chan)
+
+    sentinal(DataContainer, q_channel, res_channel_dict, status_chan)
+
+`sentinal()` waits for a item to be put on the q_channel.
+
+The item put in `q_channel` should either be a `String` with the message `"Done"`, which will cause `sentinal()` to exit it's  while loop and return or the following **Query Dict**.
+
+- **Query Dict**: this Dict will contain 3 keys: `"query_f"`, `"query_args"`, and `"client"`. The primary function of `sentinal()` is to call, `q_res = query_req["query_f"](DataContainer, query_req["query_args"]...)` and place the result `q_res` in `res_channel_dict[query_req["client"]]`
+
+# Arguments
+- `DataContainer::Any`: This should be a container. In typical usage it'll be a DataFrame that was loaded from deployDataStore()
+- `q_channel::Channel`: A single channel that will take!() wait for a query dict to be put!() on it.
+- `res_channel_dict::Dict{Any, Channel}`: Indexed by worker id. Will be used to look up which channel to place query result on
+- `status_chan::Channel`: Status Messages are put!() on this channel
+
+# Returns
+- `Nothing`
+
+# Throws
+- `Nothing`
+"""
+function sentinal(DataContainer, q_channel, res_channel_dict, status_chan)
     stat_dict = Dict("message" => "In sentinal",
                      "t_idx" => 1,
                      "myid()" => myid())
@@ -61,6 +92,7 @@ function sentinal(DataContainer, q_channel,res_channel_dict, status_chan)
     end
 end
 
+
 """
 """
 function query_client(q_channels, res_channel_dict, agrigate_f, query_f, query_args...)
@@ -77,6 +109,7 @@ function query_client(q_channels, res_channel_dict, agrigate_f, query_f, query_a
     res = agrigate_f(res_list...)
     return res
 end
+
 
 function make_query_channels(data_worker_pool, proc_worker_pool, chan_depth::Int=5)
     proc_chan = Dict(p => RemoteChannel(()->Channel{Any}(chan_depth), p) for p ∈ proc_worker_pool)
