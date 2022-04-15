2f9b6b52d23b111d3b3c0670c9a2b17cc71224ef
Albert Bode <abode@illinois.edu>
2022-04-15 11:35:44 -0500

    Inital commit of clean working functions with docs, run_test code isn't included yet.

---
+++ src/DQhelpers.jl
@@ -0,0 +1,53 @@
+#DQHelpers.jl
+module Helpers
+### I'm unsure if you need to  be "using" these modules in both files or not
+using Distributed
+using Serialization
+using DataFrames
+
+"""
+loadSerializedFiles(partitioned_file_list)
+
+Example/Helper function for loading serialized files using the deploydatastore
+
+# Arguments
+- `partitioned_file_list::Any`: Dictionary of arrays where eacch key is a host id and the array is a list of files to be loaded.
+
+# Returns
+- `Dict{Any, Any}`: Returns a .
+"""
+function loadSerializedFiles(partitioned_file_list)
+	for file in serialized_file_list[myid()]
+		df = deserialize()
+	end
+	return df
+end
+
+"""
+partition(data, hosts)
+
+Splits the data into a dictionary of non-overlapping equal sized chunks with the host id as the key of each partition
+
+# Arguments
+- `data::Any`: Array of worker ids that each of the data partitions will be placed on
+- `hosts::Any`: Array of host ids
+
+# Returns
+- `Dict{Any, Any}`: Returns a Ditionary with worker id as the key, and partition of the data.
+"""
+function partition(data, hosts)
+	hosts_dict = Dict()
+	chunk_size = Int(ceil(size(data,1)/size(hosts,1)))
+	chunk = 0
+	for host_id in hosts
+		if (chunk+1)*chunk_size < size(data,1)
+			hosts_dict[host_id] = data[(chunk*chunk_size+1):((chunk+1)*chunk_size)]
+		else
+			hosts_dict[host_id] = data[(chunk*chunk_size+1):end]
+		end
+		chunk += 1
+	end
+	return hosts_dict
+end
+
+end # module Helpers
